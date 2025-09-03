062edcedc1b6f2b3bd005a4dea9e7385607a7713
Aram Hammoudeh <aram.hammoudeh@indr.com>
2025-09-03 17:54:34 -0600

    test: verify husky hooks are working

---
+++ test-husky.js
@@ -0,0 +1,6 @@
+// Test file to verify Husky hooks
+const message = "This is a test file for Husky hooks";
+const formattedCode = "Now properly formatted";
+
+// Using the variables to avoid unused warnings
+export { message, formattedCode };
